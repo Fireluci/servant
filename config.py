7ba51300d23bff66e5f0d4c87a1ad4188631e1ac
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-06-10 12:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filestore")
 
 
 
-SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "easysky.in")
-SHORTLINK_API = os.environ.get('SHORTLINK_API', "a3f19acc6e58669565e8a3eb4cd5f7b040db4989")
+SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "linkcents.com")
+SHORTLINK_API = os.environ.get('SHORTLINK_API', "5d46b172f013af7ea5d6d3b1a18ab4640166a8fa")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
